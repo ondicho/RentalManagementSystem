72cfe33786eeff6927b8965d426dde3ea819b55e
ondicho <ondicho53@gmail.com>
2020-12-10 01:36:01 +0300

    creates a new activity tenantInformation

---
+++ app/src/main/java/com/moringa/rentalmanagementsystem/TenantInformationActivity.java
@@ -0,0 +1,14 @@
+package com.moringa.rentalmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TenantInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenant_information);
+    }
+}
