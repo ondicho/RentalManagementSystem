56dbc034d6a1d333f3f59f298ed05411c4a59f69
ondicho <ondicho53@gmail.com>
2020-12-10 01:56:20 +0300

    butterknifes all the cardviews in dashboard activity and attaches click listeners that navigate to their respective activities

--- app/src/main/java/com/moringa/rentalmanagementsystem/DashboardActivity.java
+++ app/src/main/java/com/moringa/rentalmanagementsystem/DashboardActivity.java
@@ -2,13 +2,50 @@ package com.moringa.rentalmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class DashboardActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.tenantInformationImageView) ImageView tenantImageView;
+    @BindView(R.id.rentStatusImageView) ImageView statusImageView;
+    @BindView(R.id.confirmPaymentImageView) ImageView paymentImageView;
+    @BindView(R.id.aboutUsImagemageView) ImageView aboutUsImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        ButterKnife.bind(this);
+
+        tenantImageView.setOnClickListener(this);
+        statusImageView.setOnClickListener(this);
+        paymentImageView.setOnClickListener(this);
+        aboutUsImageView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==tenantImageView){
+            Intent intent=new Intent(DashboardActivity.this,TenantInformationActivity.class);
+            startActivity(intent);
+        }
+        if(v==statusImageView){
+            Intent intent=new Intent(DashboardActivity.this,RentStatusActivity.class);
+            startActivity(intent);
+        }
+        if(v==paymentImageView){
+            Intent intent=new Intent(DashboardActivity.this,ConfirmPaymentActivity.class);
+            startActivity(intent);
+        }
+        if(v==aboutUsImageView){
+            Intent intent=new Intent(DashboardActivity.this,AboutUsActivity.class);
+            startActivity(intent);
+        }
     }
 }
