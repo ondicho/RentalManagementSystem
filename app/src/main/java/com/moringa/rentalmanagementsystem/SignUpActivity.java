4063e2a5578d8379b8ba09f71d42e50c35dcb650
ondicho <ondicho53@gmail.com>
2020-12-09 17:07:34 +0300

    sets an on click listener on loginTextView in SignUpactivity that points to loginActivity

--- app/src/main/java/com/moringa/rentalmanagementsystem/SignUpActivity.java
+++ app/src/main/java/com/moringa/rentalmanagementsystem/SignUpActivity.java
@@ -2,13 +2,36 @@ package com.moringa.rentalmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.nameEditText) EditText mNameEditText;
+    @BindView(R.id.apartmentNumberEditText) EditText mApartmentNumberEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmpasswordEditText;
+    @BindView(R.id.signUpButton) Button mSignUpButton;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==mLoginTextView){
+            Intent intent=new Intent(SignUpActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
