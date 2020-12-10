84ef1e8c4dcbcd3a74c9c88bc849e2a88ab2981d
ondicho <ondicho53@gmail.com>
2020-12-10 14:37:50 +0300

    uses firebase methods to authenticate user during log in

--- app/src/main/java/com/moringa/rentalmanagementsystem/LoginActivity.java
+++ app/src/main/java/com/moringa/rentalmanagementsystem/LoginActivity.java
@@ -1,19 +1,30 @@
 package com.moringa.rentalmanagementsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String TAG = LoginActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
 
     @BindView(R.id.emailEditText) EditText mEmailEditText;
     @BindView(R.id.passwordEditText) EditText mPasswordEditText;
@@ -26,10 +37,39 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        mAuth=FirebaseAuth.getInstance();
+
         mSignUpTextView.setOnClickListener(this);
         mLoginButton.setOnClickListener(this);
     }
 
+    private void loginWithPassword() {
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+
+        if (email.equals("")) {
+            mEmailEditText.setError("please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("password cannot be blank");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "signInWithEmail", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
     @Override
     public void onClick(View v) {
         if(v==mSignUpTextView){
@@ -37,8 +77,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             startActivity(intent);
         }
         if(v==mLoginButton){
-            Intent intent=new Intent(LoginActivity.this,DashboardActivity.class);
-            startActivity(intent);
+            loginWithPassword();
         }
     }
 }
