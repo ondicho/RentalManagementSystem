2ea95dc129f554329cade64c92ec791d72030ac9
ondicho <ondicho53@gmail.com>
2020-12-10 14:16:54 +0300

    sets up  user authentication with firebase in signup activity

--- app/src/main/java/com/moringa/rentalmanagementsystem/SignUpActivity.java
+++ app/src/main/java/com/moringa/rentalmanagementsystem/SignUpActivity.java
@@ -1,19 +1,31 @@
 package com.moringa.rentalmanagementsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG=SignUpActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+
 
     @BindView(R.id.emailEditText) EditText mEmailEditText;
     @BindView(R.id.nameEditText) EditText mNameEditText;
@@ -30,8 +42,31 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
 
+        mAuth=FirebaseAuth.getInstance();
+
         mLoginTextView.setOnClickListener(this);
         mSignUpButton.setOnClickListener(this);
+    }
+
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String apartmentNumber = mApartmentNumberEditText.toString().trim();
+        String password = mPasswordEditText.toString().trim();
+        String confirmPassword = mConfirmpasswordEditText.toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Log.d(TAG,"Authentication successful");
+                                } else{
+                                    Toast.makeText(SignUpActivity.this,"Authentication failed",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
     }
 
     @Override
@@ -41,8 +76,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             startActivity(intent);
         }
         if(v==mSignUpButton){
-            Intent intent=new Intent(SignUpActivity.this,DashboardActivity.class);
-            startActivity(intent);
+           createNewUser();
         }
     }
 }
