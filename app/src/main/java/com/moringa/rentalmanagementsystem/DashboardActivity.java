96eac01799d36e91ee3656015903c4d10c501c7a
ondicho <ondicho53@gmail.com>
2020-12-14 00:05:04 +0300

    creates a new toggle drawer in the dashboard activity

--- app/src/main/java/com/moringa/rentalmanagementsystem/DashboardActivity.java
+++ app/src/main/java/com/moringa/rentalmanagementsystem/DashboardActivity.java
@@ -1,6 +1,8 @@
 package com.moringa.rentalmanagementsystem;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
@@ -21,6 +26,9 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
     @BindView(R.id.rentStatusImageView) ImageView statusImageView;
     @BindView(R.id.confirmPaymentImageView) ImageView paymentImageView;
     @BindView(R.id.aboutUsImagemageView) ImageView aboutUsImageView;
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_view) NavigationView navigationView;
+    @BindView(R.id.toolbar2) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,11 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         statusImageView.setOnClickListener(this);
         paymentImageView.setOnClickListener(this);
         aboutUsImageView.setOnClickListener(this);
+
+//        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
@@ -54,23 +67,23 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_logout) {
-            logout();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_main, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_logout) {
+//            logout();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     private void logout() {
         FirebaseAuth.getInstance().signOut();
