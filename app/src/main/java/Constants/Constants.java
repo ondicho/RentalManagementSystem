e5fde0bc878b4b7272c5f8dcf60ac355fe2cd524
ondicho <ondicho53@gmail.com>
2021-05-24 17:49:18 +0300

    adds tenancy agreement field to main menu

--- app/src/main/java/Constants/Constants.java
+++ app/src/main/java/Constants/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
     public static final String FIREBASE_CHILD_SEARCHED_PAYMENTS= "payments";
     public static final  String FIREBASE_CHILD_PAYMENTS="payments";
     public static final String FIREBASE_CHILD_USERS="users";
+
+    public static final String MPESA_C2B_RESOURCE_URL="https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
+
 }
