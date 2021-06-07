1a2b72b28be5c626b572e385b8bd1fc76d30ca26
ondicho <ondicho53@gmail.com>
2021-06-07 03:36:58 +0300

    adds a okhttp client method to C2B client class

--- app/src/main/java/payment/C2BApi.java
+++ app/src/main/java/payment/C2BApi.java
@@ -1,5 +1,17 @@
 package payment;
 
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 public interface C2BApi {
+
+    @GET("oauth/v1/generate?grant_type=client_credentials")
+    Call<AccessToken> getAccessToken();
+
+
+    @POST("mpesa/stkpush/v1/processrequest")
+    Call<STKPush> getMPesa(@Body STKPush STKPush);
 
 }
--- app/src/main/java/payment/C2BClient.java
+++ app/src/main/java/payment/C2BClient.java
@@ -1,8 +1,42 @@
 package payment;
 
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 
 public class C2BClient {
     private static Retrofit retrofit = null;
+    private static  C2BApi C2BApi=null;
+    private static STKPush STKPush=null;
+
+    public static STKPush getApi(String SANDBOX_BASE_URL,String DARAJA_CONSUMER_KEY) throws IOException {
+        if(STKPush == null){
+            OkHttpClient client = new OkHttpClient();
+
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create(mediaType,
+//                    "{\"ShortCode\":\" \",
+//                    \"CommandID\":\"CustomerPayBillOnline\",
+//                    \"Amount\":\" \",
+//                    \"Msisdn\":\" \",
+//                    \"BillRefNumber\":\" \" }"
+            );
+            Request request = new Request.Builder()
+                    .url("https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate")
+                    .post(body)
+                    .addHeader("authorization", "Bearer ACCESS_TOKEN")
+                    .addHeader("content-type", "application/json")
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+        }
+        return STKPush;
+    }
 
 }
